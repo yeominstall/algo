76933318122d3fcdecb6857c638038e78f5d0312
Yeomin Nam <y.minn13@gmail.com>
2024-03-27 00:40:07 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false);
+	long long a, b, c;
+	cin >> a >> b >> c;
+	cout << a + b + c << '\n';
+
+	return 0;
+}
