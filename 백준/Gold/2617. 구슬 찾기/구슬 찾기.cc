0dc2a6e3bfd4e639682a8999ade266dbe6e3bca9
Yeomin Nam <y.minn13@gmail.com>
2024-05-20 00:00:55 +0900

    [Gold IV] Title: 구슬 찾기, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<vector<int>> balls;
+
+int main(void)
+{
+    cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+    const int base = 0xff;
+    cin >> n >> m;
+    balls.resize(n + 1, vector<int>(n + 1, base));
+    int i, j, k;
+    int heavy, light;
+    for (i = 1; i <= n; ++i)
+        balls[i][i] = 0;
+    for (i = 0; i < m; ++i)
+    {
+        cin >> heavy >> light;
+        balls[heavy][light] = 1;
+    }
+    for (k = 1; k <= n; ++k)
+    {
+        for (i = 1; i <= n; ++i)
+        {
+            if (i == k) continue;
+            for (j = 1; j <= n; ++j)
+            {
+                if (j == k || i == j) continue;
+                if (balls[i][j] == 1 || balls[i][k] == base || balls[k][j] == base) continue;
+                balls[i][j] = 1;
+            }
+        }
+    }
+    int total = 0;
+    for (i = 1; i <= n; ++i)
+    {
+        int rowcnt = 0, colcnt = 0;
+        for (j = 1; j <= n; ++j)
+        {
+            if (balls[i][j] == 1) ++rowcnt;
+            if (balls[j][i] == 1) ++colcnt;
+        }
+        if (rowcnt > n / 2 || colcnt > n / 2)
+            ++total;
+    }
+    cout << total << '\n';
+
+    return 0;
+}
