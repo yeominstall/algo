a8c1e4c2d23557f8b3ae126781c22e880646b524
Yeomin Nam <y.minn13@gmail.com>
2024-06-03 01:00:58 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+    int x[3], y[3];
+    for (int i = 0; i < 3; ++i)
+        cin >> x[i] >> y[i];
+    if (x[0] == x[1]) cout << x[2] << ' ';
+    else if (x[0] == x[2]) cout << x[1] << ' ';
+    else cout << x[0] << ' ';
+    if (y[0] == y[1]) cout << y[2] << ' ';
+    else if (y[0] == y[2]) cout << y[1] << ' ';
+    else cout << y[0] << ' ';
+
+    return 0;
+}
